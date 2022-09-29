a697dbbe6343b232e80e22ed06438593e77b6b98
Eloi Pampliega <eloipampliega@gmail.com>
2022-09-29 16:09:46 +0200

    2o commit, implementamos musiquita + jugabilidad

--- app3.js
+++ app3.js
@@ -1,6 +1,7 @@
 const sprite = new Image();
 sprite.src = 'img/imagenes.png'
-
+const backmusic = new Audio('img/focus-loop-corporate-music-114297.mp3');
+backmusic.volume = 0.2
 
 
 // function game(){
@@ -17,13 +18,14 @@ sprite.src = 'img/imagenes.png'
     const x = canvas.width / 2; //PER POSICIONAR AL MIG DE L'SCREEN
     const y = canvas.height / 2;//PER POSICIONAR AL MIG DE L'SCREEN  
 
-    class PlayerRot { //CREEM EL PERSONATGE
+    //CREEM EL PERSONATGE
+    class PlayerRot { 
         constructor(posX, posY, width, height, deg){
             this.x = posX
             this.y = posY
             this.width = width
             this.height = height
-            this.deg = deg
+            this.deg = deg // ??? ROTACIÓ???
         }
         
         draw() {
@@ -38,19 +40,17 @@ sprite.src = 'img/imagenes.png'
                 75,
                 50
             );
-            // c.translate(Math.hypot(player.x - enemy.x, player.y - enemy.y))
-            c.rotate(0);
-            // c.fill()
-            // c.beginPath();
-            // c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-            // c.fillStyle = this.color
-            // c.fill()
+            
+        }
+        rotate(){
+            this.draw()
+            c.rotate(1)
         }
 
     };
     
-
-    class Player { //CREEM EL PERSONATGE
+    //CREEM LA ALDEA
+    class Player {
         constructor(x, y, radius, color){
             this.x = x
             this.y = y
@@ -58,7 +58,7 @@ sprite.src = 'img/imagenes.png'
             this.color = color
         }
         
-        draw() {
+        draw() { //PINTEM LA ALDEA
             c.drawImage(
                 sprite,
                 0,
@@ -70,14 +70,10 @@ sprite.src = 'img/imagenes.png'
                 290,
                 145
             );
-            // c.beginPath();
-            // c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-            // c.fillStyle = this.color
-            // c.fill()
         }
     };
 
-    
+    //CREEM LES BALES
     class Projectile {
         constructor (x, y, radius, color, velocity) {
             this.x = x
@@ -87,11 +83,11 @@ sprite.src = 'img/imagenes.png'
             this.velocity = velocity
         };
 
-        draw() {
+        draw() { //DIBUIXEM LA BALA
             c.drawImage(
                 sprite,
                 1280,
-                500,
+                485,
                 1418,
                 556,
                 this.x-10,
@@ -121,7 +117,7 @@ sprite.src = 'img/imagenes.png'
             this.velocity = velocity
         };
         
-        draw() {
+        draw() { //DIBUIXEM ELS ENEMICS
             c.drawImage(
                 sprite,
                 1800,
@@ -134,68 +130,72 @@ sprite.src = 'img/imagenes.png'
                 40
             );
             c.beginPath()
-            // c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-            // c.fillStyle = this.color
-            // c.fill()
+            
         };
         
-        update() { //DISPARAR, PASSEM EL MÈTODE DRAW() I PINTEM
+        update() { //PASSEM EL MÈTODE DRAW() I PINTEM LA TRAJECTORIA
             this.draw()
             this.x = this.x + this.velocity.x
             this.y = this.y + this.velocity.y
         }
     }
-    let player = new Player(x, y, 50, "blue");
-    let playerRot = new Player(x-30, y-25, 444, 254, 0) 
+    let player;
+    let playerRot;
+
+    //ARRAYS PER GUARDAR ELS ENEMICS I LES BALES
     let projectiles = [];
     let enemies = []
     
-    function init() {
-        player = new Player(x, y, 55, "blue");
+    function init() { //PRINTEGEM LA ALDEA I EL JUGADOR
+        player = new Player(x, y, 60, "blue");
         playerRot = new PlayerRot(x-30, y-25, 444, 254, 0)
-        projectiles = [];
-        enemies = [];
-        score = 0;
+        projectiles = []; //INICIEM ELS ARRAYS A 0
+        enemies = []; //INICIEM ELS ARRAYS A 0
+        score = 0; //SCORE A 0
+        veloci = 1.5;
+        veloci1 = 5;
         document.getElementById('scoreId').innerHTML = score;
     }
 
+    let animationId;
+    let spawnId;
+    let score = 0;
 
     
-    
-    function spawnEnemies() {
-        setInterval(() => {
-            const radius = Math.random() +15
-            
+    function spawnEnemies(frames) {
+        spawnId = setInterval(() => {
+            const radius = Math.random() +15; // mínim de radi dels enemics
             let x;
             let y;
-            if (Math.random() < 0.5) {
-                x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+            if (Math.random() < 0.5) { //RESpAWN ALEATORIAMENT ¡FORA! DEL CANVAS ( WIDTH / HEIGHT+ RADIUS )
+                x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius //SPAWN ALEATORI PER LA DRETA O ESQUERRA DEL CANVAS
                 y = Math.random() * canvas.height
             } else {
                 x = Math.random() * canvas.width
-                y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+                y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius // SpAWN ALEATORI PER SOTA O SOBRE EL CANVAS
             }
+            //IF x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+            // && y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius    TOTS ELS ENEMICS ES CREERAN EN LES CANTONADES DEL CANVAS.
             const color = `hsl(${Math.random()*360}, 100%, 50%)`
             
+            //RESTEM DES DEL DESTÍ FINS LORIGEN, NO DEL REVES
             const angle = Math.atan2(canvas.height/2 - y, 
             canvas.width/2 - x)
             const velocity = {
-                x: Math.cos(angle) * 1.5, 
-                y: Math.sin(angle) * 1.5
+                x: Math.cos(angle) * veloci, 
+                y: Math.sin(angle) * veloci
             }
             enemies.push(new Enemy(x, y, radius, color, velocity))
-            
-            console.log(enemies)
-        }, 1500)
+            console.log(frames);
+        }, frames)
     }
     
-    let animationId;
-    let score = 0;
-    
-    
+    let veloci = 1.5
+    let veloci1 = 5;
+
     function animate(){
         animationId = requestAnimationFrame(animate);
-        
+
         //ESBORRA EL RASTRE DE LES BALES
         c.clearRect(0, 0, canvas.width, canvas.height) 
         player.draw();
@@ -207,12 +207,17 @@ sprite.src = 'img/imagenes.png'
         enemies.forEach((enemy, k) => {
             enemy.update();
             
-            //END GAME
+            //END GAME: COLISION ENEMIGO CON JUGADOR
             const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
             if (dist - enemy.radius - (player.radius - 2) < 1){
                 cancelAnimationFrame(animationId);
+                enemies.forEach((enemy) => delete enemy);
+                enemies = [];
+                veloci = 1.5;
+                veloci1 = 5;
                 document.getElementById('fixedScore').innerHTML = score;
                 divFix.style.display = 'flex';
+                clearInterval(spawnId);
             }
             
             //COLISION ENEMIGO / BALA
@@ -223,6 +228,8 @@ sprite.src = 'img/imagenes.png'
                     
                     score += 100;
                     document.getElementById('scoreId').innerHTML = score;
+                    veloci += 0.08; //PER CADA ENEMIC ELIMINAT: EL SEGUENT VA MÉS RAPID FINS AL CENTRE
+                    veloci1 += 0.04; //PER CADA ENEMIC ELIMINAT: LA TEVA BALA VA MÉS RAPID, PERÒ ESCALA MENYS QUE LA VELOCITAT DE L'ENEMIC
                     
                     setTimeout(() => {
                         enemies.splice(k, 1)
@@ -233,18 +240,22 @@ sprite.src = 'img/imagenes.png'
         })
     }
     
+
+    //DISPARAR LA BALA AMB EL CLICK
     addEventListener('click', (event) =>{
-        const angle = Math.atan2(event.clientY - y, event.clientX - x)
+        const angle = Math.atan2(event.clientY - y, event.clientX - x) // L'ANGLE DES DEL CENTRE DEL CANVAS FINS ON ES FA EL CLICK PER DISPARAR.
         const velocity = {
-            x: Math.cos(angle) * 5, 
-            y: Math.sin(angle) * 5
+            x: Math.cos(angle) * veloci1, 
+            y: Math.sin(angle) * veloci1
         }
         projectiles.push(new Projectile(x, y, 6.5, 'red', velocity))
     });
     
+    //COMENÇAR EL JOC, INICIEM FUNCIO INIT + ANIMATE + SPAWNENEMIES + DIV
     startGameId.addEventListener('click', () => {
         init();
         animate();
-        spawnEnemies();
-        divFix.style.display = 'none'
+        spawnEnemies(1500);
+        divFix.style.display = 'none';
+        backmusic.play();
     })
